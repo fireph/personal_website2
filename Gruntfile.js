bf003b58551a873c61224244d508fbe40a9c89cd
fireph <443370+fireph@users.noreply.github.com>
2020-11-08 00:02:30 -0800

    use uglify compress and mangle to make index.html smaller

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,53 @@
-module.exports = function(grunt) {
-  const jimp = require('jimp');
-  const glob = require('glob');
+module.exports = function (grunt) {
+  const jimp = require("jimp");
+  const glob = require("glob");
   const path = require("path");
 
   grunt.initConfig({
-    jsResources: ['echo.min.js', 'modernizr-custom.js', 'master.js', 'google-analytics.js'],
+    jsResources: [
+      "echo.min.js",
+      "modernizr-custom.js",
+      "master.js",
+      "google-analytics.js",
+    ],
     clean: {
-      webp: ['webp/img/projects/*'],
-      tiny: ['tiny/img/projects/*']
+      webp: ["webp/img/projects/*"],
+      tiny: ["tiny/img/projects/*"],
     },
     cwebp: {
       dynamic: {
         options: {
-          q: 50
+          q: 50,
         },
-        files: [{
-          expand: true,
-          src: ['img/projects/*'],
-          dest: 'webp/'
-        }]
-      }
+        files: [
+          {
+            expand: true,
+            src: ["img/projects/*"],
+            dest: "webp/",
+          },
+        ],
+      },
     },
     stylus: {
       compile: {
         options: {
-          compress: true
+          compress: true,
         },
         files: {
-          'master.css': ['master.styl']
-        }
-      }
+          "master.css": ["master.styl"],
+        },
+      },
     },
     uglify: {
+      options: {
+        compress: true,
+        mangle: true,
+      },
       dist: {
         files: {
-          'build/combined.js': ['<%= jsResources %>']
-        }
-      }
+          "build/combined.js": ["<%= jsResources %>"],
+        },
+      },
     },
     replace: {
       dist: {
@@ -45,120 +56,144 @@ module.exports = function(grunt) {
             {
               //Grab the <!--build-resume-start--> and <!--build-resume-end--> comments and everything in-between
               match: /\<\!\-\-build\-resume\-start[\s\S]*build\-resume\-end\-\-\>/,
-              replacement: '<%= grunt.file.read("resume/index.html") %>'
+              replacement: '<%= grunt.file.read("resume/index.html") %>',
             },
             {
               //Grab the <!--build-js-start--> and <!--build-js-end--> comments and everything in-between
               match: /\<\!\-\-build\-js\-start[\s\S]*build\-js\-end\-\-\>/,
-              replacement: '<script><%= grunt.file.read("build/combined.js") %></script>'
+              replacement:
+                '<script><%= grunt.file.read("build/combined.js") %></script>',
             },
             {
               //Grab the <!--build-css-start--> and <!--build-css-end--> comments and everything in-between
               match: /\<\!\-\-build\-css\-start[\s\S]*build\-css\-end\-\-\>/,
-              replacement: '<style><%= grunt.file.read("master.css") %></style>'
-            }
-          ]
+              replacement:
+                '<style><%= grunt.file.read("master.css") %></style>',
+            },
+          ],
         },
         files: [
           {
             expand: true,
             flatten: true,
-            src: ['index-dev.html'],
-            dest: 'build'
-          }
-        ]
-      }
+            src: ["index-dev.html"],
+            dest: "build",
+          },
+        ],
+      },
     },
     htmlmin: {
       dist: {
         options: {
           removeComments: true,
-          collapseWhitespace: true
+          collapseWhitespace: true,
         },
         files: {
-          'index.html': 'build/index-dev.html',
-        }
+          "index.html": "build/index-dev.html",
+        },
       },
     },
     connect: {
       server: {
         options: {
           port: 8081,
-          keepalive: true
-        }
-      }
+          keepalive: true,
+        },
+      },
     },
     watch: {
       stylus: {
-        files: ['master.styl'],
-        tasks: ['stylus', 'replace', 'htmlmin']
+        files: ["master.styl"],
+        tasks: ["stylus", "replace", "htmlmin"],
       },
       js: {
-        files: ['<%= jsResources %>'],
-        tasks: ['uglify', 'replace', 'htmlmin']
+        files: ["<%= jsResources %>"],
+        tasks: ["uglify", "replace", "htmlmin"],
       },
       html: {
-        files: ['index-dev.html', 'resume/index.html'],
-        tasks: ['replace', 'htmlmin']
+        files: ["index-dev.html", "resume/index.html"],
+        tasks: ["replace", "htmlmin"],
       },
       img: {
-        files: ['img/projects/*'],
-        tasks: ['clean', 'jimp', 'cwebp']
-      }
+        files: ["img/projects/*"],
+        tasks: ["clean", "jimp", "cwebp"],
+      },
     },
     concurrent: {
       options: {
-        logConcurrentOutput: true
+        logConcurrentOutput: true,
       },
-      target: ['connect', 'watch']
-    }
+      target: ["connect", "watch"],
+    },
   });
 
-  function resizeDirectoryImages(srcPath, dstPath, { width = jimp.AUTO, height = jimp.AUTO }) {
+  function resizeDirectoryImages(
+    srcPath,
+    dstPath,
+    { width = jimp.AUTO, height = jimp.AUTO }
+  ) {
     return new Promise((resolve, reject) => {
-      glob(srcPath + "*.@(png|jpg|jpeg|bmp)", { nocase: true, nodir: true, realpath: true}, (err, files) => {
-        if(err) {
-          reject(err);
-        } else {
-          resolve(files);
+      glob(
+        srcPath + "*.@(png|jpg|jpeg|bmp)",
+        { nocase: true, nodir: true, realpath: true },
+        (err, files) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(files);
+          }
         }
-      });
-    }).then(files => {
-      return Promise.all(files.map(filepath => {
-        return new Promise((resolve, reject) => {
-          return jimp.read(filepath).then(image => {
-            let newFilePath = path.join(dstPath, path.basename(filepath))
-            image
-              .resize(width, height)
-              .write(newFilePath, (err) => {
-                if(err) {
+      );
+    }).then((files) => {
+      return Promise.all(
+        files.map((filepath) => {
+          return new Promise((resolve, reject) => {
+            return jimp.read(filepath).then((image) => {
+              let newFilePath = path.join(dstPath, path.basename(filepath));
+              image.resize(width, height).write(newFilePath, (err) => {
+                if (err) {
                   reject(err);
                 } else {
                   resolve(newFilePath);
                 }
               });
-          })
-        }).then(console.log)
-      }));
+            });
+          }).then(console.log);
+        })
+      );
     });
   }
 
-  grunt.registerTask('jimp', 'Jimp resize for low-res previews of project images.', function() {
-    resizeDirectoryImages('img/projects/', 'tiny/img/projects/', { width: 16 })
-      .then(() => {
-        console.log('Done!');
+  grunt.registerTask(
+    "jimp",
+    "Jimp resize for low-res previews of project images.",
+    function () {
+      resizeDirectoryImages("img/projects/", "tiny/img/projects/", {
+        width: 16,
+      }).then(() => {
+        console.log("Done!");
       });
-  });
+    }
+  );
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-cwebp');
-  grunt.loadNpmTasks('grunt-contrib-stylus');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-replace');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-cwebp");
+  grunt.loadNpmTasks("grunt-contrib-stylus");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-replace");
+  grunt.loadNpmTasks("grunt-contrib-htmlmin");
+  grunt.loadNpmTasks("grunt-contrib-connect");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-concurrent");
 
-  grunt.registerTask('default', ['clean', 'jimp', 'cwebp', 'stylus', 'uglify', 'replace', 'htmlmin', 'concurrent:target']);
+  grunt.registerTask("default", [
+    "clean",
+    "jimp",
+    "cwebp",
+    "stylus",
+    "uglify",
+    "replace",
+    "htmlmin",
+    "concurrent:target",
+  ]);
 };
